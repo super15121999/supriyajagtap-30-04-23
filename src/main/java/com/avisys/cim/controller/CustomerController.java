ff9ad9548dc615698b27357cfb40e71e630bd0bb
supriya <jagtapsupriya1999@gmail.com>
2023-04-30 20:56:30 +0530

    Neccesspory changes are done in controller and in service class also

--- src/main/java/com/avisys/cim/controller/CustomerController.java
+++ src/main/java/com/avisys/cim/controller/CustomerController.java
@@ -4,14 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.avisys.cim.entity.Customer;
+import com.avisys.cim.entity.MobileNumber;
 import com.avisys.cim.service.CustomerCreationException;
 import com.avisys.cim.service.CustomerService;
 
@@ -38,6 +41,21 @@ public class CustomerController {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
   }
+  
+  // Modify the application to support multiple mobile number for a single customer
+  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
+      Customer customer = customerService.getCustomerById(id);
+      return ResponseEntity.ok(customer);
+  }
+
+  // Modify the application to be able to save a customer with multiple mobile number over REST API
+  @PostMapping(value = "/{id}/mobile-numbers", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Customer> addMobileNumbersToCustomer(@PathVariable Long id, @RequestBody List<String> mobileNumbers) {
+      Customer customer = customerService.addMobileNumber(id, (MobileNumber) mobileNumbers);
+      return ResponseEntity.ok(customer);
+  }
+
   
 }
   
--- src/main/java/com/avisys/cim/service/CustomerService.java
+++ src/main/java/com/avisys/cim/service/CustomerService.java
@@ -1,11 +1,13 @@
 package com.avisys.cim.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.avisys.cim.entity.Customer;
+import com.avisys.cim.entity.MobileNumber;
 import com.avisys.cim.repository.CustomerRepository;
 
 @Service
@@ -29,4 +31,23 @@ public class CustomerService {
 	    customer.setMobileNumber(customerDto.getMobileNumber());
 	    return customerRepository.save(customer);
 	  }
+  public Customer addMobileNumber(Long customerId, MobileNumber mobileNumber) {
+      Customer customer = customerRepository.findById(customerId).orElseThrow(() -> new NoSuchElementException("Customer not found"));
+      customer.getMobileNumbers().add(mobileNumber);
+      mobileNumber.setCustomer(customer);
+      return customerRepository.save(customer);
+  }
+
+  public void removeMobileNumber(Long customerId, Long mobileNumberId) {
+      Customer customer = customerRepository.findById(customerId).orElseThrow(() -> new NoSuchElementException("Customer not found"));
+      customer.getMobileNumbers().removeIf(mobileNumber -> mobileNumber.getId().equals(mobileNumberId));
+      customerRepository.save(customer);
+    }
+
+
+public Customer getCustomerById(Long id) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
 }
